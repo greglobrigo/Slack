4b45072a6efb7c020db30dfd456a6fcb53624cf6
John Carreos <jrcarreos1@gmail.com>
2021-09-26 10:33:30 +0800

    Changes in chat

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -48,82 +48,164 @@ export default function PermanentDrawerLeft() {
             </span>
           </Typography>
         </Toolbar>
-        <textarea className="msg-area" value="sample message" />
       </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        <Drawer
-          sx={{
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              boxSizing: "border-box",
-            },
-          }}
-          variant="permanent"
-          anchor="left"
-        >
-          <div className="profile-pic">Profile pic area</div>
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <div className="profile-pic">Profile pic area</div>
 
-          <Divider />
-          <List>
-            <ListItemButton className="list-item">
-              <ListItemIcon>
-                <PeopleAltIcon />
-              </ListItemIcon>
-              <ListItemText primary="All Users" />
-            </ListItemButton>
-            <ListItemButton onClick={handleClickOpenChannel}>
-              <ListItemIcon>
-                <ForumIcon />
-              </ListItemIcon>
-              <ListItemText primary="Channels" />
-              {openChannel ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={openChannel} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemText primary="Channel 1" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemText primary="Channel 2" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemText primary="Channel 3" />
-                </ListItemButton>
-              </List>
-            </Collapse>
-          </List>
+        <Divider />
+        <List>
+          <ListItemButton className="list-item">
+            <ListItemIcon>
+              <PeopleAltIcon />
+            </ListItemIcon>
+            <ListItemText primary="All Users" />
+          </ListItemButton>
+          <ListItemButton onClick={handleClickOpenChannel}>
+            <ListItemIcon>
+              <ForumIcon />
+            </ListItemIcon>
+            <ListItemText primary="Channels" />
+            {openChannel ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={openChannel} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="Channel 1" />
+              </ListItemButton>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="Channel 2" />
+              </ListItemButton>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="Channel 3" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
 
-          <Divider />
+        <Divider />
 
-          <List>
-            <ListItemButton onClick={handleClickOpenMessage}>
-              <ListItemIcon>
-                <ChatBubbleIcon />
-              </ListItemIcon>
-              <ListItemText primary="Direct Messages" />
-              {openMessage ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={openMessage} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemText primary="Direct Message 1" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemText primary="Direct Message 2" />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemText primary="Direct Message 3" />
-                </ListItemButton>
-              </List>
-            </Collapse>
-          </List>
-        </Drawer>
+        <List>
+          <ListItemButton onClick={handleClickOpenMessage}>
+            <ListItemIcon>
+              <ChatBubbleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Direct Messages" />
+            {openMessage ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={openMessage} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="Direct Message 1" />
+              </ListItemButton>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="Direct Message 2" />
+              </ListItemButton>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="Direct Message 3" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          bgcolor: "background.default",
+          pt: 10,
+          height: "20%",
+        }}
+      >
+        <div className="messages-container">
+          <p className="chat-area">
+            111111111Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Cupiditate error eos eveniet qui laborum doloremque porro,
+            voluptate, omnis sunt at iure neque praesentium temporibus odit
+            voluptates quis voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+          <p className="chat-area">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            error eos eveniet qui laborum doloremque porro, voluptate, omnis
+            sunt at iure neque praesentium temporibus odit voluptates quis
+            voluptatem recusandae suscipit?
+          </p>
+        </div>
+
+        <Box sx={{ height: "25%" }}>
+          <textarea className="msg-area" value="sample message" />
+        </Box>
       </Box>
     </Box>
   );
